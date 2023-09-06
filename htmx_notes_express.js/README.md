795aad2978a7ec3940a295c022fe938fc0d1be2f
dxps <marius.ileana@gmail.com>
2023-09-06 20:23:27 +0300

    feat: add htmx_notes_express.js

---
+++ htmx_notes_express.js/README.md
@@ -0,0 +1,16 @@
+# htmx_notes_express.js
+
+This is a demo note-taking app using markdown inspired by the [React Server Components demo](https://github.com/reactjs/server-components-demo) written using
+[htmx](https://htmx.org) and [Express.js](https://expressjs.com).
+
+![Screenshots](screenshots.gif)
+
+Forked from [jrajasegar/htmx-notes](https://github.com/rajasegar/htmx-notes/tree/main) repo.
+
+<br/>
+
+## Local development
+
+Clone the git repo, install the dependencies (`npm install`) and start the server (`npm start`).
+
+You can view the demo app in action at [http://localhost:3000](http://localhost:3000).
---
+++ htmx_notes_express.js/data/notes.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    id: 1,
+    title: 'Meeting Notes',
+    createdAt: '6:07 AM',
+    content: 'This is an example note. It contains **Markdown**!',
+  },
+  {
+    id: 2,
+    title: 'Make a thing',
+    createdAt: '6:07 AM',
+    content: `It's very easy to make some words **bold** and other words *italic* with
+Markdown. You can even [link to htmx's website!](https://htmx.org).`,
+  },
+  {
+    id: 3,
+    title: 'A note with a very long title because sometimes you need more words',
+    createdAt: '6:07 AM',
+    content: `You can write all kinds of [amazing](https://en.wikipedia.org/wiki/The_Amazing)
+notes in this app! These note live on the server in the \`notes\` folder.
+
+![This app is powered by htmx](images/htmx-logo.png)`,
+  },
+  {
+    id: 4,
+    title: 'I wrote this note today',
+    createdAt: '6:07 AM',
+    content: 'It was an excellent note'
+  },
+];
+

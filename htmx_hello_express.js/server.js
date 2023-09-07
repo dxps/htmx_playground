79276709a663224c7d60a591cbb7637d5090e0f1
dxps <marius.ileana@gmail.com>
2023-09-07 15:42:57 +0300

    feat(htmx_hello_express.js): add event example

--- htmx_hello_express.js/server.js
+++ htmx_hello_express.js/server.js
@@ -9,6 +9,13 @@ app.get('/say-hello', (req, res) => {
     res.send("Hello from the server side!")
 })
 
+app.get('/status', (req, res) => {
+    // Delay the response to give the visual indicator some time to live.
+    setTimeout(function () {
+        res.send("<img class=\"max-w-[10%]\" src=\"ok.png\" />")
+    }, 2000)
+})
+
 app.listen(3001, () => {
     console.info(`The server is active, listening on port ${PORT}`)
 })

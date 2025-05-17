ecefd5865f26e09cd7130fefdd47097776d5ee33
dxps <marius.ileana@gmail.com>
2025-05-17 17:49:49 +0300

    chore(htmx_go_template_fragments): htmx v2.0.4 upd, small refactor

--- htmx_go_template_fragments/main.go
+++ htmx_go_template_fragments/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -17,37 +18,36 @@ to wire up your application to use the correct template name and the fragment wi
 var page *template.Template
 
 // init function sets up the template+fragment.  Most of the work is actually done here.
-// In a larger program, this would likely be stored in a separate file, but this makes for a
-// simple example.
+// In a larger program, this would likely be stored in a separate file.
 func init() {
 
 	page = template.New("main")
 
-	page = template.Must(page.Parse(`<!DOCTYPE html>
-	
-	<html>
-	<head>
-		<script src="https://unpkg.com/htmx.org@1.8.0"></script>
-		<link rel="stylesheet" href="https://the.missing.style"/>
-		<title>Template Fragment Example</title>
-	</head>
-	<body>
-		<h1>Template Fragment Example</h1>
-		
-		<p>This page demonstrates how to create and serve 
-		<a href="https://htmx.org/essays/template-fragments/">template fragments</a> 
-		using the <a href="https://pkg.go.dev/text/template">built-in template package</a> in Go.</p>
-		
-		<p>This is accomplished by using the "block" action in the template, which lets you
-		define and execute a sub-template in a single step.</p>
-		<!-- Here's the fragment.  We can target it by executing the "buttonOnly" template. -->
-		{{block "buttonOnly" .}}
-			<button hx-get="/?counter={{.next}}&template=buttonOnly" hx-swap="outerHTML">
-				This Button Has Been Clicked {{.counter}} Times
-			</button>
-		{{end}}
-	</body>
-	</html>`))
+	page = template.Must(page.Parse(
+		`<!DOCTYPE html>
+		<html>
+		<head>
+			<script src="https://unpkg.com/htmx.org@2.0.4"></script>
+			<link rel="stylesheet" href="https://the.missing.style"/>
+			<title>Template Fragment Example</title>
+		</head>
+		<body>
+			<h1>Template Fragment Example</h1>
+			
+			<p>This page demonstrates how to create and serve 
+			<a href="https://htmx.org/essays/template-fragments/">template fragments</a> 
+			using the <a href="https://pkg.go.dev/text/template">built-in template package</a> in Go.</p>
+			
+			<p>This is accomplished by using the "block" action in the template, which lets you
+			define and execute a sub-template in a single step.</p>
+			<!-- Here's the fragment.  We can target it by executing the "buttonOnly" template. -->
+			{{block "buttonOnly" .}}
+				<button hx-get="/?counter={{.next}}&template=buttonOnly" hx-swap="outerHTML">
+					This Button Has Been Clicked {{.counter}} Times
+				</button>
+			{{end}}
+		</body>
+		</html>`))
 }
 
 // handleRequest does the work to execute the template (or fragment) and serve the result.
@@ -72,8 +72,9 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// main is the entry point for the program. It sets up and executes the HTTP server.
 func main() {
+
 	http.HandleFunc("/", handleRequest)
+	log.Println("Listening on http://localhost:9099 ...")
 	_ = http.ListenAndServe(":9099", nil)
 }
